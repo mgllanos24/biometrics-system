c3345a828afdd2d15c1462844098a1222845394d
mgllanos24 <mgllanos@yahoo.com>
2025-01-05 12:54:47 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/biometricsDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Define user schema and model
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    biometrics: [Array], // Array of face descriptors
+    profilePicture: String, // Base64 string or image URL
+    signature: String, // Base64 string or image URL
+    createdAt: { type: Date, default: Date.now },
+});
+const User = mongoose.model('User', userSchema);
+
+// API to register a user
+app.post('/register', async (req, res) => {
+    const { firstName, lastName, biometrics, profilePicture, signature } = req.body;
+    try {
+        const user = new User({ firstName, lastName, biometrics, profilePicture, signature });
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(500).send({ message: 'Error registering user', error });
+    }
+});
+
+// API to get all users
+app.get('/users', async (req, res) => {
+    try {
+        const us
